99fb8c2948f609cfc7c0e0ce2d3ebaa5c963de8d
ArtsiomChekh <qbartem@gmail.com>
2023-04-30 10:23:06 +0300

    Config

---
+++ src/main/java/spring/blog/controllers/UsersController.java
@@ -0,0 +1,30 @@
+package spring.blog.controllers;
+
+import spring.blog.models.User;
+import spring.blog.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UsersController {
+
+  @Autowired
+  private UserService userService;
+
+  @RequestMapping("/users")
+  public String index(Model model,
+      @PageableDefault(sort = {"userName"}, value = 5) Pageable pageable) {
+
+		Page<User> users = this.userService.findAll(pageable);
+
+    model.addAttribute("users", users);
+
+		return "users/index";
+  }
+
+}
